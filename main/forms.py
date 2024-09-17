e93ba2be1ec9de59482958c507226c7c30a8fc36
gracekarinn <gracekarina12@gmail.com>
2024-09-17 22:11:40 +0700

    feat: add ProductForm for creating new products

---
+++ main/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from main.models import ProductakhorModel
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = ProductakhorModel
+        fields = ['name', 'price', 'description', 'image', 'quantity']
