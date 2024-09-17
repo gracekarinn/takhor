2a2e38fe5dba015bebef7ffecad23a02c2880764
gracekarinn <gracekarina12@gmail.com>
2024-09-17 22:11:44 +0700

    feat: add id field to ProductakhorModel

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class ProductakhorModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=15)
     price = models.IntegerField()
     description = models.TextField()
