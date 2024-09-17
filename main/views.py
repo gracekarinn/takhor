49c8e65066cf8d5a7102a6e4caa05bc15c61a3c7
gracekarinn <gracekarina12@gmail.com>
2024-09-17 22:11:52 +0700

    feat: Add create product entry functionality and serializers for XML and JSON

--- main/views.py
+++ main/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import ProductakhorModel
+from django.shortcuts import render, redirect
+from main.forms import ProductForm
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_main(request):
-    model = ProductakhorModel.objects.all()
+    product_entries = ProductakhorModel.objects.all()
 
     product = [
         {
@@ -35,6 +39,38 @@ def show_main(request):
         'class' : 'PBP F',
         'shop': 'takhor',
         'product': product,
+        'product_entries': product_entries
     }
 
     return render(request, "main.html", context)
+
+def create_product_entry(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('main:show_main')
+    else:
+        form = ProductForm()
+    context = {'form': form}
+    return render(request, 'create_product_entry.html', context)
+
+def show_xml(request):
+    product_entries = ProductakhorModel.objects.all()
+    data = serializers.serialize('xml', product_entries)
+    return HttpResponse(data, content_type='application/xml')
+
+def show_json(request):
+    product_entries = ProductakhorModel.objects.all()
+    data = serializers.serialize('json', product_entries)
+    return HttpResponse(data, content_type='application/json')
+
+def show_xml_by_id(request, id):
+    product_entries = ProductakhorModel.objects.get(id=id)
+    data = serializers.serialize('xml', [product_entries])
+    return HttpResponse(data, content_type='application/xml')
+
+def show_json_by_id(request, id):
+    product_entries = ProductakhorModel.objects.get(id=id)
+    data = serializers.serialize('json', [product_entries])
+    return HttpResponse(data, content_type='application/json')
