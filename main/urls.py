92a72e9f254df6a7fffc58c9e1c3bbb7af991711
gracekarinn <gracekarina12@gmail.com>
2024-09-18 10:28:47 +0700

    Refactor URL patterns to use string instead of integer for id parameter

--- main/urls.py
+++ main/urls.py
@@ -10,19 +10,8 @@ urlpatterns = [
     path('create/', create_product_entry, name='create_product_entry'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
-<<<<<<< HEAD
-<<<<<<< HEAD
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
-=======
-    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
-
->>>>>>> 39772c8 (feat: Add new URL patterns for creating and displaying products)
-=======
-    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
->>>>>>> 3c36296 (Refactor URL patterns to use string instead of integer for id parameter)
 ]
 
 if settings.DEBUG:
