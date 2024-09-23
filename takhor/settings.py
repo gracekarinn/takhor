b1b65602a4a3a821752eebb616b63a844aea03cf
gracekarinn <gracekarina12@gmail.com>
2024-09-23 14:21:10 +0700

    chore: Update settings.py for media file handling

--- takhor/settings.py
+++ takhor/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,6 +125,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
  
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "grace-karina31-takhor.pbp.cs.ui.ac.id"]
 
